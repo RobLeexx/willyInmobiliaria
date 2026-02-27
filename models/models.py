d75ac7fb1a4d09af6e7a7c90330642c989c4e329
Roberto Carlos Millán <72417732+RobLeexx@users.noreply.github.com>
2026-02-27 15:16:16 +0100

    test debug

--- models/models.py
+++ models/models.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
 
+    test_debug_field = fields.Char(string="Test Debug Field")
